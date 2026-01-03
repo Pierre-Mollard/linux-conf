96a558f82a29e4f164dbd2e8c33d6fec16e51cea
Pierre Mollard <58636597+Pierre-Mollard@users.noreply.github.com>
2026-01-03 23:23:23 +0100

    feat: better nvim transparency + install fix + test other env

--- dotfiles/nvim/config/nvim/lua/plugins/catppuccin.lua
+++ dotfiles/nvim/config/nvim/lua/plugins/catppuccin.lua
@@ -4,29 +4,53 @@ return {
     "catppuccin",
     priority = 1000,
     opts = {
+      lsp_styles = {
+        underlines = {
+          errors = { "undercurl" },
+          hints = { "undercurl" },
+          warnings = { "undercurl" },
+          information = { "undercurl" },
+        },
+      },
       transparent_background = true,
+      -- Some popups (like Lazy) need explicit override
+      custom_highlights = function(colors)
+        return {
+          NormalFloat = { bg = "NONE" },
+          FloatBorder = { bg = "NONE" },
+          FloatTitle = { bg = "NONE" },
+          -- Lazy.nvim specific
+          LazyNormal = { bg = "NONE" },
+          MasonNormal = { bg = "NONE" },
+          Pmenu = { bg = "NONE" }, -- Autocomplete menu
+          PmenuSel = { bg = colors.surface1, fg = "NONE" },
+          TelescopeNormal = { bg = "NONE" },
+          TelescopeBorder = { bg = "NONE" },
+        }
+      end,
       integrations = {
         aerial = true,
         alpha = true,
         cmp = true,
         dashboard = true,
         flash = true,
+        fzf = true,
+        grug_far = true,
         gitsigns = true,
+        headlines = true,
         illuminate = true,
         indent_blankline = { enabled = true },
         leap = true,
         lsp_trouble = true,
         mason = true,
-        markdown = true,
         mini = true,
-        native_lsp = { enabled = true },
+        navic = { enabled = true, custom_bg = "lualine" },
         neotest = true,
-        neotree = true, -- File explorer transparency
+        neotree = true,
         noice = true,
         notify = true,
-        semantic_tokens = true,
+        snacks = true,
         telescope = true,
-        treesitter = true,
         treesitter_context = true,
         which_key = true,
       },
--- scripts/install.sh
+++ scripts/install.sh
@@ -4,16 +4,25 @@ SCRIPT_DIR="$(cd "$(dirname "$SCRIPT_PATH")" && pwd)"
 DOTFILES="$SCRIPT_DIR/../dotfiles"
 
 # Create inital .config file
-if [ -d "~/.config" ]; then
-  mkdir ~/.config
-fi
-
-if [ -d "~/.config/tmux" ]; then
-  mkdir ~/.config/tmux
-fi
+mkdir -p ~/.config
+mkdir -p ~/.config/tmux
+mkdir -p ~/.config/tmux/plugins
+mkdir -p ~/.config/nvim
+mkdir -p ~/.tmux
+mkdir -p ~/.vim
+mkdir -p ~/.vim/colors
 
 ln -sf $DOTFILES/vim/vimrc ~/.vimrc
+ln -sf $DOTFILES/vim/vim/colors/catppuccin_frappe.vim ~/.vim/colors/catppuccin_frappe.vim
 ln -sf $DOTFILES/tmux/tmux.conf ~/.tmux.conf
+ln -sf $DOTFILES/git/gitconfig ~/.gitconfig
+
+if [ ! -d ~/.config/tmux/plugins/catppuccin ]; then
+  mkdir -p ~/.config/tmux/plugins/catppuccin
+  git clone -b v2.1.3 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
+fi
+
+cp -rf $DOTFILES/nvim/config/* ~/.config
 
 #TODO: automate with loop, make bash distro aware, handle the clangd special conf for esp idf.
 #TODO: maybe remove the double software name in this repo /dotfiles?
